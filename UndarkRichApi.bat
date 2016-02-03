766cc5eacb6d4edfceca542d34612a9525bd413f
saunders77 <michaelsaunders@outlook.com>
2016-02-03 14:37:21 -0800

    removed unniccessaey fules

--- UndarkRichApi.bat
+++
@@ -1,2 +0,0 @@
-reg add HKEY_CURRENT_USER\Software\Microsoft\Office\16.0\Common\FeatureControl /v Enable /t REG_DWORD /d 0x301 /f
-echo ^<Clearance xmlns="http://schemas.microsoft.com/office/nexus/2013/07/flightcontrol"^>^<Enabled^>^<Feature Id="35902997" /^>^</Enabled^>^<Disabled /^>^</Clearance^> > %localappdata%\Microsoft\Office\clearance.xml
